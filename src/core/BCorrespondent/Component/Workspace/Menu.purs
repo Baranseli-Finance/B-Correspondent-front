676054192804493e6b37bd49ae1806470ec4b1a4
Sergey Yakovlev <fclaw007@gmail.com>
2023-10-09 13:41:27 +0300

    menu

--- src/core/BCorrespondent/Component/Workspace/Menu.purs
+++ src/core/BCorrespondent/Component/Workspace/Menu.purs
@@ -9,6 +9,10 @@ import Halogen.HTML as HH
 import Halogen.HTML.Properties.Extended as HPExt
 import Type.Proxy (Proxy(..))
 import Halogen.HTML.Events (onClick)
+import Data.Enum (class Enum, class BoundedEnum, fromEnum)
+import Data.Generic.Rep (class Generic)
+import Data.Enum.Generic (genericSucc, genericPred, genericCardinality, genericToEnum, genericFromEnum)
+import Data.Bounded (class Bounded)
 
 proxy = Proxy :: _ "workspace_menu"
 
@@ -20,19 +24,40 @@ data Output = Dashboard | History
 
 data Action = OpenDashboard | OpenHistory
 
+derive instance genericAction :: Generic Action _
+derive instance eqAction :: Eq Action
+derive instance ordAction :: Ord Action
+
+instance Enum Action where
+  succ = genericSucc
+  pred = genericPred
+
+instance Bounded Action where
+  top = OpenDashboard
+  bottom = OpenHistory
+
+instance BoundedEnum Action where
+  cardinality = genericCardinality
+  toEnum = genericToEnum
+  fromEnum = genericFromEnum
+
+type State = { selected :: Int }
+
 component =
   H.mkComponent
-    { initialState: identity
-    , render: const render
+    { initialState: const { selected: 0 }
+    , render: render
     , eval: H.mkEval H.defaultEval
       { handleAction = handleAction }
     }
     where
-      handleAction OpenDashboard = H.raise Dashboard
-      handleAction OpenHistory = H.raise History
+      handleAction OpenDashboard = 
+        H.modify_ _ { selected = fromEnum OpenDashboard } *> H.raise Dashboard
+      handleAction OpenHistory = 
+        H.modify_ _ { selected = fromEnum OpenHistory } *> H.raise History
 
-render = 
-  HH.div [css "workspace-menu"] 
+render { selected } = 
+  HH.div [css "workspace-menu"]
   [
       HH.div_
       [
@@ -41,12 +66,16 @@ render =
           [ 
               HH.li_
               [ 
-                HH.div [onClick (const OpenDashboard)] [ HH.i [css "fa fa-home", HPExt.style "font-size:30px;color:white; cursor:pointer"] [] ]
+                HH.div [onClick (const OpenDashboard)] [ HH.i [css "fa fa-home", HPExt.style ("font-size:30px;color:white;" <> mkCursor 0 selected)] [] ]
               ]
           ,   HH.li [HPExt.style "padding-top:30px"]
               [ 
-                HH.div [onClick (const OpenHistory)] [ HH.i [css "fa fa-history", HPExt.style "font-size:30px;color:white; cursor:pointer"] [] ]
+                HH.div [onClick (const OpenHistory)] [ HH.i [css "fa fa-history", HPExt.style ("font-size:30px;color:white;" <> mkCursor 1 selected)] [] ]
               ]    
           ]
       ]
   ]
+
+mkCursor :: Int -> Int -> String
+mkCursor idx a | idx == a = "cursor: not-allowed"
+               | otherwise = "cursor:pointer"
